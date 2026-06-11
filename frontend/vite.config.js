3777ce43d3de9893eaa87a8605d1a442f4b85bc7
hfenelsoftllc <hfenelsoftllc@outlook.com>
2026-06-10 21:02:06 -0400

    fix frontend and backend connection

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,13 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
   build: {
     outDir: 'dist',
